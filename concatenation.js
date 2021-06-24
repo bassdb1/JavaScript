72a0270b0b5a78668493d85183cb7592c4be17cd
bassdb1 <d-bass@outlook.com>
2021-06-24 10:23:58 -0400

    Add files via upload

---
+++ concatenation.js
@@ -0,0 +1,12 @@
+// Add numbers with the `+` operator
+console.log(2 + 10);
+
+// Concatenate strings using the same operator
+console.log("bubble" + "gum");
+
+// Don't forget to include spaces to separate words
+console.log("hello " + "world");
+
+// Something curious will happen if you add a number to a string
+console.log(3 + "3");
+console.log("Windows" + 95);
