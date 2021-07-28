e96ac954fe83530f11202b4795554d41baf9957e
bassdb1 <d-bass@outlook.com>
2021-07-28 11:33:25 -0400

    Using the Length Property

---
+++ length.js
@@ -0,0 +1,14 @@
+// Adding `.length` to the end of array
+
+// This will not list the contents of the array, but just the lengtgh of the array
+console.log([99, 100, 101].length);
+
+// This will list the contents of the array
+console.log([99, 100, 101]);
+
+// Adding `.length` on to a variable that contains
+// an array works, too. Cool!
+var letters = ["a", "b", "c"];
+console.log(letters.length);
+
+
